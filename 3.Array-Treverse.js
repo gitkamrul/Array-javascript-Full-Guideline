5fb5a837f1c7f7e7661919a43e159a637023e77e
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 20:48:12 +0600

    travers file create

---
+++ 3.Array-Treverse.js
@@ -0,0 +1,19 @@
+
+
+var arr = [1,2,3,4,5,6,7,8,9];
+var sum = 0;
+
+// Simple Travers Array
+for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]);   
+}
+
+
+// Addition Traverse Array 
+
+for (let i = 0; i < arr.length; i++) {
+  sum+=arr[i]
+}
+console.log(sum);
+
+
