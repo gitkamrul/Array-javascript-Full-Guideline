9ae04ad4c48edc7cd94067f7d25b3a1cf67b6365
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 18:21:13 +0600

    constructor and  factoryh file create

---
+++ Constuctor-and-factory-array.js
@@ -0,0 +1,21 @@
+
+
+// Constructor Array ;
+const newArray = new Array();
+console.log(newArray, newArray.length);     // Output: [] 0 ;
+
+// Array length Declaire 
+const newArray2 = new Array(5);
+console.log(newArray2, newArray2.length);     // Output: [ <5 empty items> ] 5
+
+
+const  arr = new Array(1,2,3,4,5,6);
+console.log(arr,arr.length);    //Output:  [ 1, 2, 3, 4, 5, 6 ] 6
+
+// Facotry Array 
+const  arr =  Array(1,2,3,4,5,6);
+console.log(arr,arr.length);    //Output:  [ 1, 2, 3, 4, 5, 6 ] 6
+
+
+
+//  Constuctor-and-factory-array.js
