5856b0c5cdeb405881432d9d3b17b91fe0cb6c6d
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 21:16:09 +0600

    Array Travers file  full  create

--- 3.Array-Treverse.js
+++ 3.Array-Treverse.js
@@ -5,15 +5,27 @@ var sum = 0;
 
 // Simple Travers Array
 for (let i = 0; i < arr.length; i++) {
-   console.log(arr[i]);   
+   console.log(arr[i]);     // Output: 1 2 3 4 5 6 7 8 9
+   
 }
 
 
 // Addition Traverse Array 
-
 for (let i = 0; i < arr.length; i++) {
   sum+=arr[i]
 }
-console.log(sum);
+console.log(sum);     //Output: 45;
 
 
+
+// Get 0large Number   in a Array 
+let largeNumber = arr[0];
+for (let i = 1; i < arr.length; i++) {
+   if(arr[i] >largeNumber){
+      largeNumber = arr[i];
+   }
+      
+}
+console.log(largeNumber);        // output: 9;
+
+//3.Array-Treverse.js
