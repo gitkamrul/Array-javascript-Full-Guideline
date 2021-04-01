19b5c76bf0be3c0070dcd72a8bf39249c5c1329f
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 17:53:54 +0600

    Array literate again change code

--- 1.Array-literate.js
+++ 1.Array-literate.js
@@ -1,15 +1,34 @@
 // Array Declire 
 var arr  = [1,2,3,4,5,6];
-console.log(arr);       //Output: 
+console.log(arr);       //Output: [ 1, 2, 3, 4, 5, 6 ]
 
 // otherwise Array Declaire 
 
 var arr1 = [];
+arr1[0]=9;
 arr1[1]=10;
 arr1[2]=11;
 arr1[3]=12;
 arr1[4]=13;
 arr1[5]=14;
-console.log(arr1);    // Output:
+console.log(arr1);    // Output: [ 9, 10, 11, 12, 13, 14 ]
 
 // 1.Array-literal.js
+
+
+var names = [
+   'kamrul',
+   'hasan',
+   'alamin'
+];
+console.log(names);      //Output: [ 'kamrul', 'hasan', 'alamin' ];
+
+names[3]= 'mahmudul';
+names[names.length] = 'rifat';
+console.log(names);   // Output: [ 'kamrul', 'hasan', 'alamin', 'mahmudul', 'rifat' ]
+
+
+
+
+
+
