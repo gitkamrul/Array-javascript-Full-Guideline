a2c5158ffe477f71b441b3f7d2f1e9ebf392ae72
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 17:47:58 +0600

    array literate file change code

---
+++ 1.Array-literate.js
@@ -0,0 +1,15 @@
+// Array Declire 
+var arr  = [1,2,3,4,5,6];
+console.log(arr);       //Output: 
+
+// otherwise Array Declaire 
+
+var arr1 = [];
+arr1[1]=10;
+arr1[2]=11;
+arr1[3]=12;
+arr1[4]=13;
+arr1[5]=14;
+console.log(arr1);    // Output:
+
+// 1.Array-literal.js
